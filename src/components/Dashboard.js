5b2d0e9e28e63eec06017dd52bd7914779af8ad9
Alex Ritzcovan <aritzcovan@gmail.com>
2020-03-11 17:31:39 -0400

    a simple text change to test amazon web services deployment

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,7 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h3 className="center">Your Timeline</h3>
+        <h3 className="center">The Ultimate Timeline</h3>
         <ul className="dashboard-list">
           {this.props.tweetIds.map(id => (
             <li key={id}>
